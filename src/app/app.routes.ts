25f551f63866f337f6ad9fca9d0a2642e8f408cb
aura-gv <gauravkhatiwada4@gmail.com>
2023-09-08 17:00:44 +0545

    routing and header-footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((a) => a.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (a) => a.RegisterComponent
+      ),
+  },
   { path: 'home', loadComponent: () => import('./pages/home/home.component') },
 ];
